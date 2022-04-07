f08a097b9b312ff7186421cdf62cd54d4be24639
Jesse Marder <jesse.d.marder@gmail.com>
2022-04-07 13:30:50 -0500

    Update file

--- model.py
+++ model.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import seaborn as sns
 import scipy.stats as stats
+import wrangle_zillow
 
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LassoLars, LinearRegression, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures, MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -38,7 +42,9 @@ def add_features(train, validate, test):
         dataset['structure_value_bin'] = pd.qcut(dataset["structure_dollar_per_sqft"], 4, labels=["low","medium","high","very_high"])
         dataset['land_value_bin'] = pd.qcut(dataset["land_dollar_per_sqft"], 4, labels=["low","medium","high","very_high"])
         dataset['has_old_heat'] = dataset["heatingorsystemdesc"] == 'Floor/Wall'
-        dataset['is_tax_delinquent'] = np.where(dataset.taxdelinquencyflag=='Y',1,0)
+        dataset['taxdelinquencyflag'] = np.where(dataset.taxdelinquencyflag=='Y',1,0)
+        dataset["delinquent_years"] = (17-dataset.taxdelinquencyyear)
+        # dataset = dataset.drop(columns = ['taxdelinquencyflag'])
 
     return train, validate, test
 
@@ -99,7 +105,7 @@ def perform_cluster_analysis(train_scaled, features_to_cluster, cluster_qty = 3,
     
     # Run significance test (t-test)
     overall_mean = train_scaled.abs_logerror.mean()
-    print(f"Overall mean logerror: {overall_mean}")
+    # print(f"Overall mean logerror: {overall_mean}")
     for col in set(train_scaled[cluster_name]):
         sample = train_scaled[train_scaled[cluster_name]==col]
         t, p = stats.ttest_1samp(sample.abs_logerror, overall_mean)
@@ -114,3 +120,246 @@ def plot_bar_comparison(train_scaled, feature_to_cluster, cluster_title, cluster
     plt.title(f"Mean Absolute Log Error by Cluster for {cluster_title}")
     plt.ylabel("Mean Absolute Log Error", fontsize=16)
     plt.xlabel("Cluster", fontsize=16)
+    
+    
+
+def data_scaling(train, validate, test, to_dummy, features_to_scale, columns_to_use):
+    """ Performs scaling and feature selection using recursive feature elimination. Performs operations on all three inputed data sets. Requires lists of features to encode (dummy), features to scale, and columns (features) to input to the feature elimination. """
+    
+    # Gets dummy variables
+    X_train_exp = pd.get_dummies(train, columns = to_dummy, drop_first=True)
+    
+    # Gets dummy variables for validate and test sets as well for later use
+    X_train = X_train_exp[columns_to_use]
+    X_validate = pd.get_dummies(validate, columns = to_dummy, drop_first=True)[columns_to_use]
+    X_test = pd.get_dummies(test, columns = to_dummy, drop_first=True)[columns_to_use]
+    
+    # Scale train, validate, and test sets using the scale_data function from wrangle.pu
+    X_train_scaled, X_validate_scaled, X_test_scaled = scale_data(X_train, X_validate, X_test,features_to_scale)
+    
+    # Set up the dependent variable in a datafrane
+    y_train = train[['logerror']]
+    y_validate = validate[['logerror']]
+    y_test = test[['logerror']]
+    
+    return X_train_scaled, X_validate_scaled, X_test_scaled, y_train, y_validate, y_test
+    
+def model_feature_selection(X_train_scaled, y_train, columns_to_use):
+    """ Performs scaling and feature selection using recursive feature elimination. Performs operations on all three inputed data sets. Requires lists of features to encode (dummy), features to scale, and columns (features) to input to the feature elimination. """
+    
+    # Perform Feature Selection using Recursive Feature Elimination
+    # Initialize ML algorithm
+    lm = LinearRegression()
+    # create RFE object - selects top 3 features only
+    rfe = RFE(lm, n_features_to_select=3)
+    # fit the data using RFE
+    rfe.fit(X_train_scaled[columns_to_use], y_train)
+    # get mask of columns selected
+    feature_mask = rfe.support_
+    # get list of column names
+    rfe_features = X_train_scaled[columns_to_use].iloc[:,feature_mask].columns.tolist()
+    # view list of columns and their ranking
+
+    # get the ranks
+    var_ranks = rfe.ranking_
+    # get the variable names
+    var_names = X_train_scaled[columns_to_use].columns.tolist()
+    # combine ranks and names into a df for clean viewing
+    rfe_ranks_df = pd.DataFrame({'Var': var_names, 'Rank': var_ranks})
+    # sort the df by rank
+    rfe_ranks_df.sort_values('Rank')
+
+    return rfe_features
+    
+def model(X_train_scaled, X_validate_scaled, X_test_scaled, y_train, y_validate, y_test, rfe_features, show_test = False, print_results = True):
+    """ Fits data to different regression algorithms and evaluates on validate (and test if desired for final product). Outputs metrics for each algorithm (r2, rmse) as a Pandas DataFrame. """
+    
+    y_train['log_error'] = y_train['logerror']
+    y_validate['log_error'] = y_validate['logerror']
+    y_test['log_error'] = y_test['logerror']
+
+
+    ### BASELINE
+    
+    # 1. Predict log_error_pred_mean
+    log_error_pred_mean = y_train['log_error'].mean()
+    y_train['log_error_pred_mean'] = log_error_pred_mean
+    y_validate['log_error_pred_mean'] = log_error_pred_mean
+
+    # 2. compute log_error_pred_median
+    log_error_pred_median = y_train['log_error'].median()
+    y_train['log_error_pred_median'] = log_error_pred_median
+    y_validate['log_error_pred_median'] = log_error_pred_median
+
+    # 3. RMSE of log_error_pred_mean
+    rmse_train = mean_squared_error(y_train.log_error, y_train.log_error_pred_mean)**(1/2)
+    rmse_validate = mean_squared_error(y_validate.log_error, y_validate.log_error_pred_mean)**(1/2)
+    if print_results:
+        print("RMSE using Mean\nTrain/In-Sample: ", round(rmse_train, 2), 
+          "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+
+    # 4. RMSE of log_error_pred_median
+    rmse_train = mean_squared_error(y_train.log_error, y_train.log_error_pred_median)**(1/2)
+    rmse_validate = mean_squared_error(y_validate.log_error, y_validate.log_error_pred_median)**(1/2)
+
+    if print_results:
+
+        print("RMSE using Median\nTrain/In-Sample: ", round(rmse_train, 2), 
+          "\nValidate/Out-of-Sample: ", round(rmse_validate, 2))
+
+    ### OLS Linear Regression
+    
+    # create the model object
+    lm = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm.fit(X_train_scaled, y_train.log_error)
+
+    # predict train
+    y_train['log_error_pred_lm'] = lm.predict(X_train_scaled)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.log_error, y_train.log_error_pred_lm)**(1/2)
+
+    # predict validate
+    y_validate['log_error_pred_lm'] = lm.predict(X_validate_scaled)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.log_error, y_validate.log_error_pred_lm)**(1/2)
+
+    if print_results:
+        print("RMSE for OLS using LinearRegression\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+    
+    # predict test
+    if show_test:
+        
+        y_test['log_error_pred_lm'] = lm.predict(X_test_scaled)
+        rmse_test = mean_squared_error(y_test.log_error, y_test.log_error_pred_lm)**(1/2)
+
+    # Lasso-Lars
+    
+    # create the model object
+    lars = LassoLars(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lars.fit(X_train_scaled, y_train.log_error)
+
+    # predict train
+    y_train['log_error_pred_lars'] = lars.predict(X_train_scaled)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.log_error, y_train.log_error_pred_lars)**(1/2)
+
+    # predict validate
+    y_validate['log_error_pred_lars'] = lars.predict(X_validate_scaled)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.log_error, y_validate.log_error_pred_lars)**(1/2)
+
+    if print_results:
+
+        print("RMSE for OLS using LarsLasso\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+
+    # predict test
+    if show_test:
+        
+        y_test['log_error_pred_lars'] = lars.predict(X_test_scaled)
+        rmse_test = mean_squared_error(y_test.log_error, y_test.log_error_pred_lars)**(1/2)
+
+#     # Tweedie
+    
+#     # create the model object
+#     glm = TweedieRegressor(power=1, alpha=0)
+
+#     # fit the model to our training data. We must specify the column in y_train, 
+#     # since we have converted it to a dataframe from a series! 
+#     glm.fit(X_train_scaled, y_train.log_error)
+
+#     # predict train
+#     y_train['log_error_pred_glm'] = glm.predict(X_train_scaled)
+
+#     # evaluate: rmse
+#     rmse_train = mean_squared_error(y_train.log_error, y_train.log_error_pred_glm)**(1/2)
+
+#     # predict validate
+#     y_validate['log_error_pred_glm'] = glm.predict(X_validate_scaled)
+
+#     # evaluate: rmse
+#     rmse_validate = mean_squared_error(y_validate.log_error, y_validate.log_error_pred_glm)**(1/2)
+    
+#     if print_results:
+#         print("RMSE for GLM using Tweedie, power=1 & alpha=0\nTraining/In-Sample: ", rmse_train, 
+#           "\nValidation/Out-of-Sample: ", rmse_validate)
+        
+#     # predict test
+#     if show_test:
+        
+#         y_test['log_error_pred_glm'] = glm.predict(X_test_scaled)
+#         rmse_test = mean_squared_error(y_test.log_error, y_test.log_error_pred_glm)**(1/2)
+
+    # Polynomial features
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=2,interaction_only=True)
+
+    # fit and transform X_train_scaled
+    X_train_degree2 = pf.fit_transform(X_train_scaled)
+
+    # transform X_validate_scaled & X_test_scaled
+    X_validate_degree2 = pf.transform(X_validate_scaled)
+    X_test_degree2 = pf.transform(X_test_scaled)
+    
+    # create the model object
+    lm2 = LinearRegression(normalize=True)
+
+    # fit the model to our training data. We must specify the column in y_train, 
+    # since we have converted it to a dataframe from a series! 
+    lm2.fit(X_train_degree2, y_train.log_error)
+
+    # predict train
+    y_train['log_error_pred_lm2'] = lm2.predict(X_train_degree2)
+
+    # evaluate: rmse
+    rmse_train = mean_squared_error(y_train.log_error, y_train.log_error_pred_lm2)**(1/2)
+
+    # predict validate
+    y_validate['log_error_pred_lm2'] = lm2.predict(X_validate_degree2)
+
+    # evaluate: rmse
+    rmse_validate = mean_squared_error(y_validate.log_error, y_validate.log_error_pred_lm2)**(1/2)
+    
+    if print_results:
+        print("RMSE for Polynomial Model, degrees=2\nTraining/In-Sample: ", rmse_train, 
+          "\nValidation/Out-of-Sample: ", rmse_validate)
+        
+    # predict test
+    if show_test:
+        
+        y_test['log_error_pred_lm2'] = lm2.predict(X_test_degree2)
+        rmse_test = mean_squared_error(y_test.log_error, y_test.log_error_pred_lm2)**(1/2)
+
+    results = pd.concat([
+    y_validate.apply(lambda col: r2_score(y_validate.log_error, col)).rename('r2'),
+    y_validate.apply(lambda col: mean_squared_error(y_validate.log_error, col)).rename('mse'),
+    ], axis=1).assign(
+        rmse=lambda df: df.mse.apply(lambda x: x**0.5)
+    )
+    if show_test:
+        results = pd.concat([
+        y_train.apply(lambda col: r2_score(y_train.log_error, col)).rename('r2_train'),
+        y_train.apply(lambda col: mean_squared_error(y_train.log_error, col)).rename('mse_train'),
+        y_validate.apply(lambda col: r2_score(y_validate.log_error, col)).rename('r2_validate'),
+        y_validate.apply(lambda col: mean_squared_error(y_validate.log_error, col)).rename('mse_validate'),
+        y_test.apply(lambda col: r2_score(y_test.log_error, col)).rename('r2_test'),
+        y_test.apply(lambda col: mean_squared_error(y_test.log_error, col)).rename('mse_test'),
+        ], axis=1).assign(
+            rmse_validate=lambda df: df.mse_validate.apply(lambda x: x**0.5)
+        )
+        
+        results = results.assign(rmse_train= lambda results: results.mse_train.apply(lambda x: x**0.5))
+        results = results.assign(rmse_test= lambda results: results.mse_test.apply(lambda x: x**0.5))
+    return results
+
